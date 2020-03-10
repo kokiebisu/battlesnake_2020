39ab89ecd077929b3e9f00ba79ace1e2e72915dd
Ken <kenichikona@gmail.com>
2020-03-10 13:36:46 -0700

    configured package.json

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(routes);
 
+// START
+app.post('/start', (request, response) => {
+  const resData = {
+    color: '#3d5a6c',
+    headType: 'silly',
+    tailType: 'bolt'
+  };
+
+  return response.json(resData);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
-})
+  console.log(`Server listening on port ${PORT}...`);
+});
